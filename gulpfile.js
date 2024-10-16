4929417f7c1b00ea781892d9a6d4e401c355907e
Den_Blazh <denisb53502006@gmail.com>
2024-10-16 17:42:11 +0300

    html task

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+// html task
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+});
